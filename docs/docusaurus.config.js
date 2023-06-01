fdc77131064899f49a6cf808a200ce4556a91041
gclift <gclift@gmail.com>
2023-05-31 18:44:29 -0600

    grc - initial docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -137,6 +137,10 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
+    markdown: {
+      mermaid: true,
+    },
+    themes: ['@docusaurus/theme-mermaid'],
 };
 
 module.exports = config;
